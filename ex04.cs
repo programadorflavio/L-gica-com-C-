8577abb102d460d68d81a0271fba61202a7c2593
Flávio Augusto <92134170+programadorflavio@users.noreply.github.com>
2022-04-12 10:59:08 -0300

    Create ex04.cs

---
+++ ex04.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    int A, B, SOMA;
+  A = int.Parse(Console.ReadLine());
+  B = int.Parse(Console.ReadLine());
+  SOMA = A + B;
+    Console.WriteLine(SOMA);
+  }
+}
