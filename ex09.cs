6c91edaac83c648a69805ce46157bea76a5cc485
Flávio Augusto <92134170+programadorflavio@users.noreply.github.com>
2022-04-14 08:51:42 -0300

    Create ex09.cs

---
+++ ex09.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+   Console.WriteLine("Qual é a distância do carro Y ?");
+     int d = int.Parse(Console.ReadLine());
+     int cal = d * 2;
+    Console.WriteLine ("A distância entre o carro X e Y é de :" + cal + "km");
+    
+    
+  }
+}
